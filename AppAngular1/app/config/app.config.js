44853f8642758858d0cc0c58e60332f3ad7f6b73
Sebastião Martins Pereira <thiagoedanielamartins@gmail.com>
2016-04-03 15:59:14 -0300

    appAngular

---
+++ AppAngular1/app/config/app.config.js
@@ -0,0 +1,37 @@
+﻿(
+    function () {
+
+        //não deixa x=criar variaveis sem a declaração var;
+        'use strict';
+        angular.module('app').config(config);
+
+
+        //injeção de dependências;
+        config.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+        function config($stateProvider, $urlRouterProvider) {
+
+            //setamos a rota padrão
+            $$urlRouterProvider.otherwise('/listar');
+            //url:'layout'
+            $$stateProvider.
+            state('layout', {
+                templateUrl: 'app/views/layout.html',
+                abstract: true,
+
+            }).
+            state('layout.cadastrar', {
+                templateUrl: 'app/views/cadastrar.html',
+                url: '/Cadastra',
+                cache: false,
+
+            }).
+            state('layout.listar', {
+                templateUrl: 'app/views/listar.html',
+                url: '/Listar',
+                cache: false,
+
+            });
+        }
+    }
+)();
---
+++ AppAngular1/app/modules/app.module.js
@@ -0,0 +1,6 @@
+﻿(
+    function () {
+        //criação do modulo e importação do u.router
+        angular.module('app', ['ui.router']);
+    }
+)();
