daf68332ce79c3017a8cf15f37bb4daec073ffab
Sebastião Martins Pereira <thiagoedanielamartins@gmail.com>
2016-06-12 22:29:01 -0300

    Js - Avançado.
    lista de negociações.

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -5,21 +5,31 @@ class NegociacaoController{
       this._inputData = $('#data');
       this._inputQuantidade = $('#quantidade');
       this._inputValor = $('#valor');
+      this._listNegociacoes = new ListNegociacoes();
   }
 
   adiciona(event){
         event.preventDefault();
 
-        let data = new Date(...this._inputData.value
-                           .split('-')
-                           .map((v,i) => v - i % 2));
+        this._listNegociacoes.adiciona(this._criarNegociacao());
+        this._limpaFormulario();
 
-       let negociacao = new Negociacao(
-            data,
+        console.log(this._listNegociacoes.negociacoes);
+  };
+
+   _limpaFormulario() {
+        this._inputData.value = '';
+        this._inputData.focus();
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0
+
+    };
+
+    _criarNegociacao(){
+         return new Negociacao(
+            DataHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
         );
-
-        console.log(negociacao);
-  }
+    };
 }
