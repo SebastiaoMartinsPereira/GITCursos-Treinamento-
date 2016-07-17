49c141dc6a4ad108ab7c16329df274e901c04e2f
Sebastião Martins Pereira <thiagoedanielamartins@gmail.com>
2016-07-17 11:01:37 -0300

    removed

--- AppAngular1/app/constants/app.constants.js
+++
@@ -1,9 +0,0 @@
-﻿(
-    function () {
-
-        //não deixa x=criar variaveis sem a declaração var;
-        'use strict';
-
-        angular.module('app').constant('url', 'http://sisamigos.azurewebsites.net/api/');
-    }
-)();
--- AppAngular1/app/modules/app.module.js
+++
@@ -1,6 +0,0 @@
-﻿(
-    function () {
-        //criação do modulo e importação do u.router
-        angular.module('app', ['ui.router']);
-    }
-)();
